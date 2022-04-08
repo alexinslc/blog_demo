83d4811ffa2d324b172093500ec4c5b256c1a4bc
Alex Lutz <alexinslc@gmail.com>
2022-04-08 22:01:03 +0000

    fixes devise and turbo, adds login

--- app/controllers/turbo_devise_controller.rb
+++ app/controllers/turbo_devise_controller.rb
@@ -1,5 +1,5 @@
 class TurboDeviseController < ApplicationController
-    class Responder < ApplicationController::Responder
+    class Responder < ActionController::Responder
       def to_turbo_stream
         controller.render(options.merge(formats: :html))
       rescue ActionView::MissingTemplate => e
@@ -8,7 +8,7 @@ class TurboDeviseController < ApplicationController
         elsif has_errors? && default_action
           render rendering_options.merge(formats: :html, status: :unprocessable_entity)
         else
-          redirect_to_navigation_location
+          redirect_to navigation_location
         end
       end
     end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ class TurboFailureApp < Devise::FailureApp
   end
 
   def skip_format?
-    %w[html turbo_stream].include? request_format.to_us
+    %w[html turbo_stream].include? request_format.to_s
   end
 end
 
