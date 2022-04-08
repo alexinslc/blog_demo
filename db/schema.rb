81acd612bbb57d288329c05dd08a5eadec53f50d
Alex Lutz <alexinslc@gmail.com>
2022-04-08 22:59:55 +0000

    adds user profile, user profile views, adds tabnine to devcontainer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_221348) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_223640) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_221348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
