a0ee072ed6850b1ee72389e4228d344e96ea012f
Alex Lutz <alexinslc@gmail.com>
2025-10-25 18:54:27 -0600

    Upgrade Ruby from 3.1.1 to 3.3.6 (#246)
    Ruby 3.3.6 upgrade completed successfully. All acceptance criteria met.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_223640) do
+ActiveRecord::Schema[7.2].define(version: 2022_04_08_223640) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
