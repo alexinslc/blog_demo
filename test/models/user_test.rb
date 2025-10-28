5b13f7530d90b61830bc4a0609065f755e507a8c
Navaneeth Krishnan P <33243337+navaneethkp36@users.noreply.github.com>
2025-10-28 08:37:31 +0530

    Destroy associated posts when a user is destroyed (#287)
    * Add dependent; :destroy for user posts
    
    * Add test to check if related posts are destroyed when a user is destroyed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should destroy associated posts when user is destroyed' do
+    user = users(:one)
+    post = posts(:one)
+
+    assert_equal user, post.user
+    assert_difference 'Post.count', -1 do
+      user.destroy
+    end
+  end
 end
