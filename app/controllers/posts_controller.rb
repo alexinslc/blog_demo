df5ceacafdd269ddd17895fd7d430e5e60a51a17
navaneethkp36 <navaneethkp36@gmail.com>
2025-10-31 23:06:07 +0530

    Add kept scope to query in set_post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,7 +65,7 @@ class PostsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:id])
+    @post = Post.kept.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
